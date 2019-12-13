f649714b13eced6f17de9a72fa296a9306f02cd4
ianstewart1 <46875198+ianstewart1@users.noreply.github.com>
2019-12-13 21:57:46 +0100

    Snapchat added

--- app/src/main/java/us/mzhang/fuse/SocialIntent.kt
+++ app/src/main/java/us/mzhang/fuse/SocialIntent.kt
@@ -4,24 +4,41 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import androidx.core.content.ContextCompat.startActivity
+
+
 
 
 class SocialIntent(val context: Context) {
 
     val twitterUser = "jack"
+    val snapchatId = "testing123"
 
     fun launchTwitter() {
         try {
             val intent = Intent(
                 Intent.ACTION_VIEW,
-                Uri.parse("twitter://user?screen_name=${twitterUser}")
+                Uri.parse("twitter://user?screen_name=$twitterUser")
             )
             context.startActivity(intent)
         } catch (e: ActivityNotFoundException) {
             context.startActivity(
                 Intent(
                     Intent.ACTION_VIEW,
-                    Uri.parse("https://twitter.com/#!/${twitterUser}")
+                    Uri.parse("https://twitter.com/#!/$twitterUser")
+                )
+            )
+        }
+    }
+
+    fun launchSnapchat() {
+        try {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://snapchat.com/add/$snapchatId"))
+            intent.setPackage("com.snapchat.android")
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            context.startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://snapchat.com/add/$snapchatId")
                 )
             )
         }
