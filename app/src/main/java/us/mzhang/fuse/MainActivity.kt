5279f033be1b389b6e049ee059013ebe918b5f07
slang21 <slang21@cmc.edu>
2019-12-14 18:46:58 +0100

    started profile page functionality

--- app/src/main/java/us/mzhang/fuse/MainActivity.kt
+++ app/src/main/java/us/mzhang/fuse/MainActivity.kt
@@ -7,7 +7,10 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
+import us.mzhang.fuse.data.user
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,28 +28,75 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnProfile.setOnClickListener {
-            startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
+            openProfilePage()
+        }
+    }
+
+    private fun openProfilePage() {
+        val db = FirebaseFirestore.getInstance()
+        val usersRef = db.collection("users")
+        var query = usersRef.whereEqualTo("uid", FirebaseAuth.getInstance().uid)
+            .limit(1)
+        var currUser = user()
+        query.get().addOnSuccessListener { users ->
+            if (users.isEmpty) {
+                currUser = user(
+                    FirebaseAuth.getInstance().uid,
+                    "",
+                    "",
+                    "",
+                    "",
+                    ""
+                )
+                usersRef.add(
+                    currUser!!
+                )
+            } else {
+                Toast.makeText(this@MainActivity, "works", Toast.LENGTH_LONG).show()
+                currUser = users.toObjects(user::class.java)[0]
+            }
+            var intent = Intent(this@MainActivity, ProfileActivity::class.java)
+            intent.putExtra("USER", currUser)
+            startActivity(intent)
+        }.addOnFailureListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Sorry we are having issues opening profile right noe",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
     private fun requestNeededPermission() {
-        if (ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    android.Manifest.permission.CAMERA)) {
-                Toast.makeText(this,
-                    "I need it for QR reading", Toast.LENGTH_SHORT).show()
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    android.Manifest.permission.CAMERA
+                )
+            ) {
+                Toast.makeText(
+                    this,
+                    "I need it for QR reading", Toast.LENGTH_SHORT
+                ).show()
             }
-            ActivityCompat.requestPermissions(this,
+            ActivityCompat.requestPermissions(
+                this,
                 arrayOf(android.Manifest.permission.CAMERA),
-                101)
+                101
+            )
         } else {
             btnScan.isEnabled = true
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         when (requestCode) {
             101 -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
