9dff35b40a57c09c6797e7e802f7787f79453fa2
ianstewart1 <46875198+ianstewart1@users.noreply.github.com>
2019-12-15 22:35:32 +0100

    Launch google play if app not found

--- app/src/main/java/us/mzhang/fuse/SocialIntent.kt
+++ app/src/main/java/us/mzhang/fuse/SocialIntent.kt
@@ -11,7 +11,7 @@ class SocialIntent(context: Context, user: User) {
 
     val context = context
     val user = user
-    
+
     fun launchIntent(mediaType: String) {
         var username = user.socialSet.get(mediaType)
         var url = ""
@@ -32,7 +32,7 @@ class SocialIntent(context: Context, user: User) {
             else if (mediaType == "instagram") intent.setPackage("com.instagram.android")
             context.startActivity(intent)
         } catch (e: ActivityNotFoundException) {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(playUrl))
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$playUrl"))
             context.startActivity(intent)
         }
     }
