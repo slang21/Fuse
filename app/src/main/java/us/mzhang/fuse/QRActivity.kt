89172b1ff2767391d48bbab48997cd52b4bdebb0
slang21 <slang21@cmc.edu>
2019-12-15 12:27:24 +0100

    passed proper info to the dialog

--- app/src/main/java/us/mzhang/fuse/QRActivity.kt
+++ app/src/main/java/us/mzhang/fuse/QRActivity.kt
@@ -1,10 +1,14 @@
 package us.mzhang.fuse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.zxing.Result
 import kotlinx.android.synthetic.main.activity_qr.*
 import me.dm7.barcodescanner.zxing.ZXingScannerView
+import us.mzhang.fuse.data.user
+import us.mzhang.fuse.dialogs.NewFriendDialog
 
 
 class QRActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
@@ -39,7 +43,26 @@ class QRActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
     override fun handleResult(qrResult: Result?) {
         test.text = qrResult?.text
+        addFriend(qrResult!!.text)
         zxingView.setResultHandler(this)
         zxingView.startCamera()
+    }
+
+    private fun addFriend(uid: String) {
+        val db = FirebaseFirestore.getInstance()
+        val usersRef = db.collection("users")
+        var query = usersRef.whereEqualTo("uid", uid)
+            .limit(1)
+        query.get().addOnSuccessListener { users ->
+            var friend = users.toObjects(us.mzhang.fuse.data.user::class.java)[0]
+            var newFriend = NewFriendDialog()
+            val bundle = Bundle()
+            bundle.putSerializable("USER", friend)
+            newFriend.arguments = bundle
+            newFriend.show(
+                supportFragmentManager,
+                "NEW_FRIEND"
+            )
+        }
     }
 }
